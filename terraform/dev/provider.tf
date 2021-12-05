6095774c09e7221fd02cbb9d6c0590240c5cab08
Sanjay Kumar <Sanjayrohilla@gmail.com>
2021-12-05 22:03:47 +1100

    m1

--- terraform/dev/provider.tf
+++ terraform/dev/provider.tf
@@ -5,6 +5,6 @@ terraform {
     backend "s3" {
         bucket = "terraform-state-bucket-2021"
         key = "app/config/dev/base-infra/terraform.tfstate"
-        region = "ap-southeast-2"
+        region = "ap-southeast-1"
     }
 }
