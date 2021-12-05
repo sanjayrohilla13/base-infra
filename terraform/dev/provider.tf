7bb073bc0c7751753cbd7a03240ac6347ff1e9cb
Sanjay Kumar <Sanjayrohilla@gmail.com>
2021-12-05 21:32:28 +1100

    s10

--- terraform/dev/provider.tf
+++ terraform/dev/provider.tf
@@ -5,7 +5,7 @@ provide "aws" {
 terraform {
     backend "s3" {
         bucket = "terraform-state-bucket-2021"
-        key = app/config/dev/base-infra/terraform.tfstate"
+        key = "app/config/dev/base-infra/terraform.tfstate"
         region = "ap-southeast-2"
     }
 }
