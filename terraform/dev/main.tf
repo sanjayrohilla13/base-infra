7b0bb629231a9d8926627ab254f9f797118a9b4f
Sanjay Kumar <Sanjayrohilla@gmail.com>
2021-12-05 21:52:17 +1100

    s2

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -2,7 +2,7 @@
 
 resource "aws_ecr_repository" "app-ecr" {
   name                 = var.ecr-name
-  image_tag_mutability = var.ecr-mutablity
+  image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
   }
--- terraform/dev/vars.tf
+++ terraform/dev/vars.tf
@@ -1,6 +1,3 @@
 variable "ecr-name" {
     value = "test-app"
 }
-variable "ecr-mutablity" {
-    value = "MUTABLE"
-}
