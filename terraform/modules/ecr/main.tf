634050eade211f021d4b822a20a7f5b0e10731d5
Sanjay Kumar <Sanjayrohilla@gmail.com>
2021-12-10 08:28:09 +1100

    removed app name from the reports

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,5 +1,6 @@
 resource "aws_ecr_repository" "ecr" {
-  name                 = "${var.application_name}/${var.name}"
+  //name                 = "${var.application_name}/${var.name}"
+  name                  = "${var.name}" 
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
