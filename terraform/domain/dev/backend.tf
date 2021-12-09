a165e1d395cccf5e40e31c656f3aae4c702583be
Sanjay Kumar <Sanjayrohilla@gmail.com>
2021-12-09 21:40:10 +1100

    added tfstate file

--- terraform/domain/dev/backend.tf
+++ terraform/domain/dev/backend.tf
@@ -1,7 +1,7 @@
-#terraform {
-#    backend "s3" {
-#        bucket = "terraform-state-bucket-2021"
-#        key = "app/config/dev/base-infra/terraform.tfstate"
-#        region = "ap-southeast-1"
-#    }
-#}
+terraform {
+    backend "s3" {
+        bucket = "terraform-state-bucket-2021"
+        key = "app/config/dev/base-infra/terraform.tfstate"
+        region = "ap-southeast-1"
+    }
+}
